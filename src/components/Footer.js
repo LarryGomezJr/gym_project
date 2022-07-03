567aeb317400d15e0ddaa31170aab9903ffe0b45
Mr. Larry <larrygomez689@gmail.com>
2022-07-03 13:05:07 +0800

    done 7/3/22

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
-
+import { Link } from 'react-router-dom';
+ 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4" flexWrap="flex">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '100px', height: '40px' }} />
+  <Stack mt="60px" bgcolor="#FFF3F4" flexWrap="wrap">
+    <Stack gap="30px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <Link to="/">
+      <img src={Logo} alt="logo" style={{ width: '100px', height: '85px', margin: '0px 20px' }} />
+    </Link>
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
-  </Box>
+    <Typography variant="h7"sx={{ fontSize: { lg: '25px', xs: '18px' } }} mt="20px" textAlign="center" pb="40px">Made by Larry </Typography>
+    
+  </Stack>
 );
 
 export default Footer;
