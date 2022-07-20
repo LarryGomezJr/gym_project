1f3d9cca579636b03948a195d42618978d3accd5
Mr. Larry <larrygomez689@gmail.com>
2022-07-20 17:13:45 +0800

    little changes in assets and components folder.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Logo from '../assets/images/Logo.png';
 import { Link } from 'react-router-dom';
  
 const Footer = () => (
-  <Stack mt="60px" bgcolor="#FFF3F4" flexWrap="wrap">
+  <Box mt="60px" bgcolor="#FFF3F4" flexWrap="wrap">
     <Stack gap="30px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <Link to="/">
       <img src={Logo} alt="logo" style={{ width: '100px', height: '85px', margin: '0px 20px' }} />
@@ -12,7 +12,7 @@ const Footer = () => (
     </Stack>
     <Typography variant="h7"sx={{ fontSize: { lg: '25px', xs: '18px' } }} mt="20px" textAlign="center" pb="40px">Made by Larry </Typography>
     
-  </Stack>
+  </Box>
 );
 
 export default Footer;
